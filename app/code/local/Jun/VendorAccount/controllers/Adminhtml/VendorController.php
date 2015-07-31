7575784af0f1a22cedff28a8a275e8724f8d0c20
Sergey D <s.dudnikov28@gmail.com>
2015-08-01 02:50:49 +0300

    added admin controller

---
+++ app/code/local/Jun/VendorAccount/controllers/Adminhtml/VendorController.php
@@ -0,0 +1,11 @@
+<?php
+
+class Jun_VendorAccount_Adminhtml_VendorController extends Mage_Adminhtml_Controller_Action
+{
+    public function indexAction() {
+        echo 'It is Controller!!!';
+        
+    }
+    
+}
+
--- app/code/local/Jun/VendorAccount/controllers/admin/VendorController.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class Jun_VendorAccount_admin_VendorCnotroller extends Mage_Adminhtml_Controller_Action
-{
-    
-}
-
